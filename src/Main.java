f8712cc4666adc0568498252e64f15ae711644dc
pobmic15 <pobmic15@gasos-ro.cz>
2026-03-13 09:00:57 +0100

    e

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] poleCisel = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        /*int[] poleCisel = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int pocetJednicek = 0;
         for (int p : poleCisel) {
             if (p == 1) {
@@ -77,6 +77,88 @@ public class Main {
         }
 
         if (kontrola) {
+            System.out.println("ANO");
+        } else {
+            System.out.println("NE");
+        }*/
+
+        int[][] pole = {
+                {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        };
+
+        int[] pocty1 = new int[9];
+        for (int i = 0; i < 9; i++) {
+            pocty1[pole[0][i] - 1]++;
+        }
+
+        boolean kontrola1 = true;
+        for (int p : pocty1) {
+            if (p != 1) {
+                kontrola1 = false;
+            }
+        }
+        if (kontrola1) {
+            System.out.println("ANO");
+        } else {
+            System.out.println("NE");
+        }
+
+        int[] pocty2 = new int[9];
+        for (int i = 0; i < 9; i++) {
+            pocty2[pole[1][i] - 1]++;
+        }
+
+        boolean kontrola2 = true;
+        for (int p : pocty2) {
+            if (p != 1) {
+                kontrola2 = false;
+            }
+        }
+        if (kontrola2) {
+            System.out.println("ANO");
+        } else {
+            System.out.println("NE");
+        }
+
+        int[][] vsechnyRadky = new int[9][9];
+        boolean kazdaKontrola1 = true;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                vsechnyRadky[i][pole[i][j] - 1]++;
+            }
+            for (int k = 0; k < 9; k++) {
+                if (vsechnyRadky[i][k] != 1) {
+                    kazdaKontrola1 = false;
+                }
+            }
+        }
+        if (kazdaKontrola1) {
+            System.out.println("ANO");
+        } else {
+            System.out.println("NE");
+        }
+
+        int[] poctySloupec = new int[9];
+        boolean kazdySloupec = true;
+        for (int j = 0; j < 9; j++) {
+            for (int i = 0; i < 9; i++) {
+                poctySloupec[pole[i][j] - 1]++;
+            }
+            for (int p : poctySloupec) {
+                if (p != 1) {
+                    kazdySloupec = false;
+                }
+            }
+        }
+        if (kazdySloupec) {
             System.out.println("ANO");
         } else {
             System.out.println("NE");
