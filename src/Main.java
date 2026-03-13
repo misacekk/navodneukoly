16a0f7e521948937c472f6bcdf5ef772374d9acc
pobmic15 <pobmic15@gasos-ro.cz>
2026-03-13 08:01:21 +0100

    e

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] poleCisel = {1,2,3,4,5,6,7,8,9};
+        int[] poleCisel = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int pocetJednicek = 0;
         for (int p : poleCisel) {
-            if (p == 1){
+            if (p == 1) {
                 pocetJednicek++;
             }
         }
 
-        if (pocetJednicek == 1){
-        System.out.println("ANO");
-    }else{
+        if (pocetJednicek == 1) {
+            System.out.println("ANO");
+        } else {
             System.out.println("NE");
         }
 
@@ -26,23 +26,60 @@ public class Main {
         int pocet9 = 0;
 
         for (int p : poleCisel) {
-            if (p == 1){pocet1++;}
-            if (p == 2){pocet2++;}
-            if (p == 3){pocet3++;}
-            if (p == 4){pocet4++;}
-            if (p == 5){pocet5++;}
-            if (p == 6){pocet6++;}
-            if (p == 7){pocet7++;}
-            if (p == 8){pocet8++;}
-            if (p == 9){pocet9++;}
+            if (p == 1) {
+                pocet1++;
+            }
+            if (p == 2) {
+                pocet2++;
+            }
+            if (p == 3) {
+                pocet3++;
+            }
+            if (p == 4) {
+                pocet4++;
+            }
+            if (p == 5) {
+                pocet5++;
+            }
+            if (p == 6) {
+                pocet6++;
+            }
+            if (p == 7) {
+                pocet7++;
+            }
+            if (p == 8) {
+                pocet8++;
+            }
+            if (p == 9) {
+                pocet9++;
+            }
         }
 
-        if (pocet1 == 1 && pocet2 == 1 && pocet3 == 1 && pocet4 == 1 && pocet5 == 1 && pocet6 == 1 && pocet7 == 1 && pocet8 == 1 && pocet9 == 1){System.out.println("ANO");}
-        else {System.out.println("NE");}
+        if (pocet1 == 1 && pocet2 == 1 && pocet3 == 1 && pocet4 == 1 && pocet5 == 1 && pocet6 == 1 && pocet7 == 1 && pocet8 == 1 && pocet9 == 1) {
+            System.out.println("ANO");
+        } else {
+            System.out.println("NE");
+        }
 
         int[] pocty = new int[9];
 
+        for (int p : poleCisel) {
+            if (p >= 1 && p <= 9) {
+                pocty[p - 1]++;
+            }
+        }
 
+        boolean kontrola = true;
+        for (int i = 0; i < pocty.length; i++) {
+            if (pocty[i] != 1) {
+                kontrola = false;
+            }
+        }
 
+        if (kontrola) {
+            System.out.println("ANO");
+        } else {
+            System.out.println("NE");
+        }
     }
 }
